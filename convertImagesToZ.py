4949bb7bd5674b12595ab9b753da4a622ca8e44f
Simen Selseng <simense@ntnu.no>
2017-02-02 15:02:28 +0100

    Added line for inverting the pixeldata

--- convertImagesToZ.py
+++ convertImagesToZ.py
@@ -1,30 +1,33 @@
 import PIL
 from PIL import Image
+from PIL import ImageOps
+
 import numpy as np
 import glob
 
 src = 'faces/'
-# outputFolder = '/processedImages/'
+outputFolder = '/processedImages/'
 basewidth = 10
 counter = 0
 
 batch_size=64
 batch_z = np.random.uniform(-1, 1, [batch_size, 100]).astype(np.float32)
 
-data = glob.glob(src + "*.jpg")
+data = glob.glob(src + "*.jpeg")
 np.random.shuffle(data)
 
 
 
 for filename in data:
     img = Image.open(filename)
-    img = img.convert('L')  # convert image to greyscale
+    # img = img.convert('L')  # convert image to greyscale
     wpercent = (basewidth / float(img.size[0]))
     hsize = int((float(img.size[1]) * float(wpercent)))
     img = img.resize((basewidth, basewidth), PIL.Image.ANTIALIAS) # resizes the image to 10x10
-    # img = img.convert('L') # convert image to black and white
-    # name = 'processedImages/'+ str(counter) + '.jpg'
-    # img.save(name)
+    img = img.convert('L') # convert image to black and white
+    name = 'processedImages/'+ str(counter) + '.jpeg'
+    # img = ImageOps.invert(img)
+    img.save(name)
 
     pix = np.array(img, np.float32)
     pix = (pix - 128) / 128 #Scales the pixels to be between -1 and 1
